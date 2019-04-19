d00d082aa185e61929f286808d4364a5c02278aa
Caradec Bisesar <c.bisesar@gmail.com>
2019-04-19 17:16:07 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from "react-dom";
-import './css/index.css';
-import registerServiceWorker from './components/registerServiceWorker';
-import Router from "./components/Router";
-
-render(<Router />, document.querySelector("#root"));
-registerServiceWorker();
