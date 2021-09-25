4f31248714957b04d73dfe243b22399fa544da11
Caradec Bisesar <c.bisesar@gmail.com>
2021-09-25 00:13:46 -0400

    Pushing debug router live

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,14 +3,27 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Bar from "./Bar";
 
+class DebugRouter extends BrowserRouter {
+  constructor(props) {
+    super(props);
+    console.log('initial history is: ', JSON.stringify(this.history, null, 2))
+    this.history.listen((location, action) => {
+      console.log(
+        `The current URL is ${location.pathname}${location.search}${location.hash}`
+      )
+      console.log(`The last navigation action was ${action}`, JSON.stringify(this.history, null, 2));
+    });
+  }
+}
+
 const Router = () => (
-  <BrowserRouter>
+  <DebugRouter>
     <Switch>
       <Route exact path="/all-about-cb/bar" component={Bar} />
       <Route exact path="/" component={App} />
       <Route component={App} />
     </Switch>
-  </BrowserRouter>
+  </DebugRouter>
 );
 
 export default Router;
