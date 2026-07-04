ca3e0f54f25db03b91aea6cccee8e1a9ffbaddb8
Caradec Bisesar <caradec@cembenchmarking.com>
2026-07-04 15:44:32 -0400

    Add /uses "The Kit" page
    Physical version of the Toolbox: camera bodies/lenses and the home
    cafe gear. Linked from the Toolbox header.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -251,7 +251,7 @@ function App() {
         </section>
 
         <section className="doc">
-          <div className="doc-head"><h2>The Toolbox</h2></div>
+          <div className="doc-head"><h2>The Toolbox</h2><span><a href="#/uses">the physical kit</a></span></div>
           <dl className="caps">
             {TOOLBOX.map(([label, items]) => (
               <div key={label} style={{ display: 'contents' }}>
